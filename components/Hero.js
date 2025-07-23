46667bd741921ddde0fa7a7c2ff008331a8cd597
Mathis <mathisgenthon@outlook.fr>
2025-07-23 13:36:31 +0200

    fixing : turbopack bug

---
+++ components/Hero.js
@@ -0,0 +1,22 @@
+"use client";
+
+// Define the Header component
+function Hero() {
+    return (
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content text-center text-secondary">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">Hello there</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+              quasi. In deleniti eaque aut repudiandae et a id nisi.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+);
+}
+
+// Export the component
+export default Hero;
